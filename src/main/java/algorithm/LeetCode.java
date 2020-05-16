1e71e7bd562af18d0bcec26859e8f1f3e123afac
nathan <1136026744@qq.com>
2020-05-16 14:40:32 +0800

    add leetcode

---
+++ src/main/java/algorithm/LeetCode.java
@@ -0,0 +1,73 @@
+package algorithm;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class LeetCode {
+    // reverse int
+    public int reverse(int x){
+        int rev = 0;
+        while (x != 0) {
+            int pop = x % 10;
+            x /= 10;
+            if (rev > Integer.MAX_VALUE/10 || (rev == Integer.MAX_VALUE / 10 && pop > 7)) return 0;
+            if (rev < Integer.MIN_VALUE/10 || (rev == Integer.MIN_VALUE / 10 && pop < -8)) return 0;
+            rev = rev * 10 + pop;
+        }
+        return rev;
+    }
+    // remove nth node from end
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+        boolean is_len_one = (head.next == null);
+        int head_order = RecursiveRemoveNthFromEnd(head, n);
+        if(is_len_one & n == 1) return null;
+        else if(head_order == n) return head.next;
+        else return head;
+    }
+    public int RecursiveRemoveNthFromEnd(ListNode node, int n){
+        if(node.next == null) return 1;
+        int this_order = RecursiveRemoveNthFromEnd(node.next, n) + 1;
+        if(this_order == n + 1) {
+            node.next = node.next.next;
+            return this_order;
+        }else return this_order;
+    }
+    //  all parenthesis permutation
+    public List<String> generateParenthesis(int n) {
+        List<String> res = new ArrayList<>();
+        StringBuilder init = new StringBuilder("(");
+        RecursiveGenerateParenthesis(init, 1, 1, 1, res, n);
+        return res;
+    }
+    public void RecursiveGenerateParenthesis(StringBuilder strb, int str_len,
+                                             int left_count, int stack_ptr, List<String> res, int n){
+        if(str_len == (2*n)) res.add(strb.toString());
+        else{
+            str_len++;
+            if(stack_ptr >= 1 & stack_ptr <= n-1 & left_count <= n-1){
+                StringBuilder t = new StringBuilder(strb);
+                strb.append("(");
+                t.append(")");
+                RecursiveGenerateParenthesis(strb, str_len, left_count+1, stack_ptr+1, res, n);
+                RecursiveGenerateParenthesis(t, str_len, left_count, stack_ptr-1, res, n);
+            }else if(stack_ptr == 0 & left_count <= n-1){
+                strb.append("(");
+                RecursiveGenerateParenthesis(strb, str_len, left_count+1, stack_ptr+1, res, n);
+            }else{
+                strb.append(")");
+                RecursiveGenerateParenthesis(strb, str_len, left_count, stack_ptr-1, res, n);
+            }
+        }
+    }
+
+
+}
+
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode(int x) {
+        val = x;
+    }
+}
