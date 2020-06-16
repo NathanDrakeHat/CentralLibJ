fc81d18594dd630d2ef8a00d96ac240c5b6edd68
nathan <1136026744@qq.com>
2020-06-16 18:42:34 +0800

    implement algorithm Bellman Ford

--- src/main/java/algorithms/graph/SSSP.java
+++ src/main/java/algorithms/graph/SSSP.java
@@ -15,7 +15,28 @@ public final class SSSP {
     }
 
     public static <T extends Comparable<T>>
-    void relax(Graph<BFS.Vertex<T>>.Edge e){
-        
+    void relax(BFS.Vertex<T> u, BFS.Vertex<T> v, Graph<BFS.Vertex<T>> G){
+        var weight = G.computeWeight(u,v);
+        if(v.d > u.d + weight){
+            v.d = u.d + weight;
+            v.parent = u;
+        }
+    }
+
+    // general case
+    public static <T extends Comparable<T>>
+    boolean algorithmBellmanFord(Graph<BFS.Vertex<T>> graph, BFS.Vertex<T> s){
+        initializeSingleSource(graph, s);
+        int len = graph.getAllVertices().toArray().length;
+        for(int i = 1; i < len; i++){
+            for(var edge : graph.getAllEdges()){
+                relax(edge.getFormerVertex(), edge.getLaterVertex(), graph);
+            }
+        }
+        for(var edge : graph.getAllEdges()){
+            if(edge.getLaterVertex().d > edge.getFormerVertex().d + graph.computeWeight(edge)){
+                return false;}
+        }
+        return true;
     }
 }
