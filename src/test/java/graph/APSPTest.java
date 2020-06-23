a805d7487940e5e1fefbd69eaf7ae10af09c2928
nathan <1136026744@qq.com>
2020-06-23 16:49:07 +0800

    add algo FloyedWarshalltest

--- src/test/java/graph/APSPTest.java
+++ src/test/java/graph/APSPTest.java
@@ -7,7 +7,13 @@ class APSPTest {
 
     @Test
     void slowAllPairsShortestPaths() {
-        var res = APSP.slowAllPairsShortestPaths(buildSlowAllPairsShortestPathsGraph());
+        var res = APSP.slowAllPairsShortestPaths(new double[][]{
+                {0,Double.POSITIVE_INFINITY,Double.POSITIVE_INFINITY,2,Double.POSITIVE_INFINITY},
+                {3,0,4,Double.POSITIVE_INFINITY,Double.POSITIVE_INFINITY},
+                {8,Double.POSITIVE_INFINITY,0,-5,Double.POSITIVE_INFINITY},
+                {Double.POSITIVE_INFINITY,1,Double.POSITIVE_INFINITY,0,6},
+                {-4,7,Double.POSITIVE_INFINITY,Double.POSITIVE_INFINITY,0}
+        });
         var answer = new double[][]{
                 {0,3,7,2,8},
                 {1,0,4,-1,5},
@@ -20,7 +26,13 @@ class APSPTest {
 
     @Test
     void fasterAllPairsShortestPaths() {
-        var res = APSP.fasterAllPairsShortestPaths(buildSlowAllPairsShortestPathsGraph());
+        var res = APSP.fasterAllPairsShortestPaths(new double[][]{
+                {0,Double.POSITIVE_INFINITY,Double.POSITIVE_INFINITY,2,Double.POSITIVE_INFINITY},
+                {3,0,4,Double.POSITIVE_INFINITY,Double.POSITIVE_INFINITY},
+                {8,Double.POSITIVE_INFINITY,0,-5,Double.POSITIVE_INFINITY},
+                {Double.POSITIVE_INFINITY,1,Double.POSITIVE_INFINITY,0,6},
+                {-4,7,Double.POSITIVE_INFINITY,Double.POSITIVE_INFINITY,0}
+        });
         var answer = new double[][]{
                 {0,3,7,2,8},
                 {1,0,4,-1,5},
@@ -30,13 +42,24 @@ class APSPTest {
         };
         assertArrayEquals(answer,res);
     }
-    double[][] buildSlowAllPairsShortestPathsGraph(){
-        return new double[][]{
+
+
+    @Test
+    void algorithmFloydWarshallTest(){
+        var res = APSP.algorithmFloydWarshall(new double[][]{
                 {0,Double.POSITIVE_INFINITY,Double.POSITIVE_INFINITY,2,Double.POSITIVE_INFINITY},
                 {3,0,4,Double.POSITIVE_INFINITY,Double.POSITIVE_INFINITY},
                 {8,Double.POSITIVE_INFINITY,0,-5,Double.POSITIVE_INFINITY},
                 {Double.POSITIVE_INFINITY,1,Double.POSITIVE_INFINITY,0,6},
                 {-4,7,Double.POSITIVE_INFINITY,Double.POSITIVE_INFINITY,0}
+        });
+        var answer = new double[][]{
+                {0,3,7,2,8},
+                {1,0,4,-1,5},
+                {-3,-4,0,-5,1},
+                {2,1,5,0,6},
+                {-4,-1,3,-2,0}
         };
+        assertArrayEquals(answer,res);
     }
 }
