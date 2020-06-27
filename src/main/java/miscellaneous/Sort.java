73fef6e2dc9f0d8bc3dbc0aba946e316e42d39af
nathan <1136026744@qq.com>
2020-06-27 21:02:37 +0800

    add iterateMergeSort

--- src/main/java/miscellaneous/Sort.java
+++ src/main/java/miscellaneous/Sort.java
@@ -1,24 +1,21 @@
 package miscellaneous;
 
 import tools.SimpleDate;
-
 import java.util.Arrays;
 import java.util.Comparator;
 import java.util.concurrent.ThreadLocalRandom;
 
 
 public final class Sort {
-    public static void mergeSort(int[] array){
-        mergeSort(array, 0, array.length);
-    }
-    private static void mergeSort(int[] array, int start, int end){
+    public static void mergeSort(double[] array){ mergeSort(array, 0, array.length); }
+    private static void mergeSort(double[] array, int start, int end){
         if ((end - start) > 1) {
             int middle = (start + end) / 2;
             mergeSort(array, start, middle); // divide
             mergeSort(array, middle, end);
             int len1 = middle - start , len2 =  end - middle ;  // merge
-            int[] left = new int[len1] ;
-            int[] right = new int[len2] ;  // copy to left and right
+            var left = new double[len1] ;
+            var right = new double[len2] ;  // copy to left and right
             System.arraycopy(array, start , left, 0, len1);
             System.arraycopy(array, middle , right, 0, len2);
 
@@ -55,7 +52,103 @@ public final class Sort {
         }// else end, because only one element
     }
 
-    private static void maxHeapify(int[] arr, int idx, int heap_size){
+    public static void iterateMergeSort(double[] array){
+        int expand_iter_times = (int) Math.floor(Math.log(array.length)/Math.log(2));
+        int group_size = 2;
+        int sub_group_size = group_size / 2;
+        int last_rest_len = 0;
+        if(Math.pow(2, expand_iter_times) != array.length){
+            last_rest_len = 1; }
+        // overall 2*k part:
+        for(int i = 0; i < expand_iter_times; i++){
+            int group_iter_times = array.length/group_size;
+            int group_start = 0;
+            double[] cache1 = new double[sub_group_size];
+            double[] cache2 = new double[sub_group_size];
+            // 2*k part:
+            for(int j = 0; j < group_iter_times; j++){
+//                System.arraycopy(array,group_start,cache1,0,sub_group_size);
+//                System.arraycopy(array,group_start+sub_group_size,cache2,0,sub_group_size);
+//                int cache1_idx = 0;
+//                int cache2_idx = 0;
+//                for(int k = group_start;(k<group_size)&&(cache1_idx<sub_group_size)&&(cache2_idx<sub_group_size); k++){
+//                    if(cache1[cache1_idx] <= cache2[cache2_idx]){
+//                        array[k] = cache1[cache1_idx++]; }
+//                    else {
+//                        array[k]  =cache2[cache2_idx++]; }
+//                }
+//                if(cache1_idx != sub_group_size){
+//                    System.arraycopy(cache1, cache1_idx,
+//                            array, group_start+cache2_idx+cache1_idx,
+//                            sub_group_size-cache1_idx); }
+//                else if(cache2_idx != sub_group_size){
+//                    System.arraycopy(cache2, cache2_idx,
+//                            array, group_start+cache2_idx+cache1_idx,
+//                            sub_group_size-cache2_idx); }
+                mergeWithCache(array, group_start, sub_group_size,group_size,cache1,cache2);
+                group_start += group_size;
+            }
+            // rest part:
+            int current_rest_len = array.length - group_iter_times*group_size;
+            if(current_rest_len > last_rest_len){
+                mergeRestPart(array,array.length-current_rest_len,
+                        current_rest_len-last_rest_len,last_rest_len);
+                last_rest_len = current_rest_len;
+            }
+
+            group_size *= 2;
+            sub_group_size /= 2;
+        }
+        // final rest part:
+        mergeRestPart(array, 0, group_size, array.length - group_size);
+    }
+    private static void mergeRestPart(double[] array, int former_start, int former_len, int later_len){
+        var cache1 = new double[former_len];
+        var cache2 = new double[later_len];
+        System.arraycopy(array, former_start, cache1, 0,former_len);
+        System.arraycopy(array, former_start+former_len, cache2, 0,later_len);
+        int cache1_idx = 0;
+        int cache2_idx = 0;
+        for(int i = former_start; (i<former_len+later_len)&&(cache1_idx<former_len)&&(cache2_idx<later_len); i++){
+            if(cache1[cache1_idx] <= cache2[cache2_idx]){
+                array[i] = cache1[cache1_idx++]; }
+            else {
+                array[i]  =cache2[cache2_idx++]; }
+        }
+        if(cache1_idx != former_len){
+            System.arraycopy(cache1, cache1_idx,
+                    array, former_start+cache2_idx+cache1_idx,
+                    former_len-cache1_idx); }
+        else if(cache2_idx != later_len){
+            System.arraycopy(cache2, cache2_idx,
+                    array, former_start+cache2_idx+cache1_idx,
+                    later_len-cache2_idx); }
+    }
+    private static void mergeWithCache(double[] array, int former_start, int len, int group_size, double[] cache1, double[] cache2){
+        System.arraycopy(array, former_start, cache1, 0,len);
+        System.arraycopy(array, former_start+len, cache2, 0,len);
+        int cache1_idx = 0;
+        int cache2_idx = 0;
+        for(int i = former_start; (i<group_size)&&(cache1_idx<len)&&(cache2_idx<len); i++){
+            if(cache1[cache1_idx] <= cache2[cache2_idx]){
+                array[i] = cache1[cache1_idx++]; }
+            else {
+                array[i]  =cache2[cache2_idx++]; }
+        }
+        if(cache1_idx != len){
+            System.arraycopy(cache1, cache1_idx,
+                    array, former_start+cache2_idx+cache1_idx,
+                    len-cache1_idx); }
+        else if(cache2_idx != len){
+            System.arraycopy(cache2, cache2_idx,
+                    array, former_start+cache2_idx+cache1_idx,
+                    len-cache2_idx); }
+    }
+    // 2 : [0,1)[1,2)  [2,3)[3,4)  [4,5)[5,6)  [6,7)[7,8) [8,9)[9,10)
+    // 4 : [0,2)[2,4)  [4,6)[6,8) | [8,10)
+    // 8 : [0,4)[4,8) | [8,10)
+
+    private static void maxHeapify(double[] arr, int idx, int heap_size){
         int l = 2 * (idx + 1);
         int l_idx = l - 1;
         int r = 2 * (idx + 1) + 1;
@@ -68,34 +161,34 @@ public final class Sort {
             max_idx = r_idx;
         }
         if (max_idx != idx) {
-            int t = arr[max_idx];
+            var t = arr[max_idx];
             arr[max_idx] = arr[idx];
             arr[idx] = t;
             maxHeapify(arr, max_idx, heap_size);
         }
     }
-    private static void buildMaxHeap(int[] arr){
+    private static void buildMaxHeap(double[] arr){
         for(int i = arr.length/2 - 1; i >= 0; i--){
             maxHeapify(arr, i, arr.length);
         }
     }
-    public static void heapSort(int[] a){
+    public static void heapSort(double[] a){
         buildMaxHeap(a);
         int heap_size = a.length;
         for(int i = a.length - 1; i >= 1; i--){
-            int t = a[0];
+            var t = a[0];
             a[0] = a[i];
             a[i] = t;
             maxHeapify(a, 0, --heap_size);
         }
     }
 
-    private static int partition(int[] a, int start, int end){ // base case (end -start)
-        int pivot = a[end - 1];
+    private static int partition(double[] a, int start, int end){ // base case (end -start)
+        var pivot = a[end - 1];
         int i = start - 1;
         for(int j = start; j < end - 1; j++){ // larger than pivot in [i+1, j-1]
             if(a[j] <= pivot){
-                int t = a[j];
+                var t = a[j];
                 a[j] = a[++i];
                 a[i] = t;
             }
@@ -104,10 +197,10 @@ public final class Sort {
         a[i] = pivot;
         return i; //pivot index
     }
-    public static void quickSort(int[] a){
+    public static void quickSort(double[] a){
         quickSort(a, 0, a.length);
     }
-    private static void quickSort(int[] a, int start, int end){
+    private static void quickSort(double[] a, int start, int end){
         if((end - start) > 1){
             int middle = partition(a, start, end);
             quickSort(a, start, middle);
@@ -115,18 +208,18 @@ public final class Sort {
         }
     }
 
-    private static int randPartition(int[] a, int start, int end){ // base case (end - start)
+    private static int randPartition(double[] a, int start, int end){ // base case (end - start)
         int pivot_idx = ThreadLocalRandom.current().nextInt(start,end);
-        int pivot = a[pivot_idx];
+        var pivot = a[pivot_idx];
 
-        int temp = a[end - 1];
+        var temp = a[end - 1];
         a[end - 1] = pivot;
         a[pivot_idx] = temp;
 
         int i = start - 1;
         for(int j = start; j < end - 1; j++){ // larger than pivot in [i+1, j-1]
             if(a[j] <= pivot){
-                int t = a[j];
+                var t = a[j];
                 a[j] = a[++i];
                 a[i] = t;
             }
@@ -135,10 +228,10 @@ public final class Sort {
         a[i] = pivot;
         return i; //pivot value position
     }
-    public static void randQuickSort(int[] a){
+    public static void randQuickSort(double[] a){
         randQuickSort(a, 0, a.length);
     }
-    private static void randQuickSort(int[] a, int start, int end){
+    private static void randQuickSort(double[] a, int start, int end){
         if((end - start) > 1){
             int middle = randPartition(a, start, end);
             randQuickSort(a, start, middle);
@@ -146,7 +239,8 @@ public final class Sort {
         }
     }
 
-    public static void countingSort(int[] a){ //int data only
+    //int data only
+    public static void countingSort(int[] a){
         int[] b = new int[a.length];
         int min = a[0], max = a[0];
         for(int i = 1; i < a.length; i++){
--- src/test/java/miscellaneous/SortTest.java
+++ src/test/java/miscellaneous/SortTest.java
@@ -1,41 +1,40 @@
 package miscellaneous;
 
 import java.util.Arrays;
+
+import org.junit.jupiter.api.Test;
 import tools.SimpleDate;
 import static org.junit.jupiter.api.Assertions.*;
 
 
 class SortTest {
-     static class Data {
-        static int[] data = new int[] {10, -5, 10, 4, 2, 6, 10, 10, 4, -9, -6, -7, 10, -7, 5, -6, 8, 8, -5, -8};
-        static int[] test1 = new int[] {-2, 4, 2, -4, -9, 6, -4, 7, -3, -8, 2, -8, 0, 10, -9, -4, -3, -6, -8, 2};
-        static int[] test2 = new int[] {-37, -8, 40, 5, -25, 15, -13, 12, 8, 44, 18, -6, -33, -37,
-                -39, -24, -14, 15, 40, 27, -15, 46, -16, -2, 4};
-        static int[] test3 = new int[] {2, 12, -24, -38, -17, -25, 5, -22, -34, 0, -18, -16, -39, -39, -36};
-        static int[] test4 = new int[] {-9, -19, -3, 16, 6, 18, -15, -20, -8, 13, -18, 13, 17, -6, 16, -2, -3};
-        static int[] test5 = new int[] {-30, -57, -55, -26, 26, -34, -8, -22, 31, -19, -49, -32, 2, 22, -8,
-                31, 31, -55, -16};
 
-        static double[] bucket_test = new double[] {0.885, 0.6497, 0.6566, 0.1263, 0.2799, 0.8863, 0.013, 0.8973,
+    static double[] test1 = new double[] {-2, 4, 2, -4, -9, 6, -4, 7, -3, -8, 2, -8, 0, 10, -9, -4, -3, -6, -8, 2};
+    static double[] test2 = new double[] {-37, -8, 40, 5, -25, 15, -13, 12, 8, 44, 18, -6, -33, -37, -39, -24, -14, 15, 40, 27, -15, 46, -16, -2, 4};
+    static double[] test3 = new double[] {2, 12, -24, -38, -17, -25, 5, -22, -34, 0, -18, -16, -39, -39, -36};
+    static int[] test4 = new int[] {-9, -19, -3, 16, 6, 18, -15, -20, -8, 13, -18, 13, 17, -6, 16, -2, -3};
+    static int[] test5 = new int[] {-30, -57, -55, -26, 26, -34, -8, -22, 31, -19, -49, -32, 2, 22, -8, 31, 31, -55, -16};
+    static int[] test6 = new int[] {10, -5, 10, 4, 2, 6, 10, 10, 4, -9, -6, -7, 10, -7, 5, -6, 8, 8, -5, -8};
+
+    static double[] bucket_test = new double[] {0.885, 0.6497, 0.6566, 0.1263, 0.2799, 0.8863, 0.013, 0.8973,
                 0.4261, 0.454, 0.7068, 0.5867, 0.1529, 0.8356, 0.7301, 0.1111, 0.8368};
-        static double[] bucket_test1 = new double[] {0.536, 0.3182, 0.7075, 0.8828, 0.3912, 0.7055, 0.1893, 0.9803,
+    static double[] bucket_test1 = new double[] {0.536, 0.3182, 0.7075, 0.8828, 0.3912, 0.7055, 0.1893, 0.9803,
                 0.5235, 0.4947, 0.6374, 0.9782};
-        static double[] bucket_test2 = new double[] {0.9224, 0.8382, 0.2349, 0.4597, 0.2921, 0.2443, 0.2077,
+    static double[] bucket_test2 = new double[] {0.9224, 0.8382, 0.2349, 0.4597, 0.2921, 0.2443, 0.2077,
                 0.8726, 0.7065, 0.2447, 0.7945, 0.2701, 0.4575, 0.1834, 0.3771};
 
-         static int[] years = new int[] {2020, 2019, 2017, 2018, 2019, 2016, 2018, 2017, 2016, 2020,
-                 2015, 2019, 2020, 2017, 2015, 2015, 2020, 2020, 2016, 2018};
-         static int[] months = new int[] {6, 9, 1, 7, 10, 11, 7, 4, 2, 7, 8, 9, 6, 3, 1, 6, 3, 4, 2, 5};
-         static int[] days = new int[] {11, 10, 11, 24, 2, 3, 8, 5, 16, 3, 9, 19, 23, 22, 1, 11, 17, 29, 15, 11};
-         static SimpleDate[] buildDate(){
-            SimpleDate[] res = new SimpleDate[20];
-            for(int i=0; i < 20; i++){
-                res[i] = new SimpleDate(years[i], months[i], days[i]);
-            }
-            return res;
+    static int[] years = new int[] {2020, 2019, 2017, 2018, 2019, 2016, 2018, 2017, 2016, 2020, 2015,
+            2019, 2020, 2017, 2015, 2015, 2020, 2020, 2016, 2018};
+    static int[] months = new int[] {6, 9, 1, 7, 10, 11, 7, 4, 2, 7, 8, 9, 6, 3, 1, 6, 3, 4, 2, 5};
+    static int[] days = new int[] {11, 10, 11, 24, 2, 3, 8, 5, 16, 3, 9, 19, 23, 22, 1, 11, 17, 29, 15, 11};
+    static SimpleDate[] buildDate() {
+        SimpleDate[] res = new SimpleDate[20];
+        for (int i = 0; i < 20; i++) {
+            res[i] = new SimpleDate(years[i], months[i], days[i]);
         }
+        return res;
     }
-     static boolean isSorted(int[] t){
+    static boolean isSorted(int[] t){
         boolean is_sorted = true;
         for(int i = 1; i < t.length; i++){
             if (t[i - 1] > t[i]) {
@@ -74,271 +73,151 @@ class SortTest {
         return is_sorted;
     }
 
-    @org.junit.jupiter.api.Test
+    @Test
     void mergeSortTest1() {
-        var copy = Data.data;
+        var copy = test1;
         var t = Arrays.copyOf(copy, copy.length);
         Sort.mergeSort(t);
         assertTrue(isSorted(t));
     }
 
-    @org.junit.jupiter.api.Test
+    @Test
     void mergeSortTest2() {
-        var copy = Data.test1;
+        var copy = test2;
         var t = Arrays.copyOf(copy, copy.length);
         Sort.mergeSort(t);
         assertTrue(isSorted(t));
     }
 
-    @org.junit.jupiter.api.Test
+    @Test
     void mergeSortTest3() {
-        var copy = Data.test2;
+        var copy = test3;
         var t = Arrays.copyOf(copy, copy.length);
         Sort.mergeSort(t);
         assertTrue(isSorted(t));
     }
 
-    @org.junit.jupiter.api.Test
-    void mergeSortTest4() {
-        var copy = Data.test3;
-        var t = Arrays.copyOf(copy, copy.length);
-        Sort.mergeSort(t);
-        assertTrue(isSorted(t));
-    }
-
-    @org.junit.jupiter.api.Test
-    void mergeSortTest5() {
-        var copy = Data.test4;
-        var t = Arrays.copyOf(copy, copy.length);
-        Sort.mergeSort(t);
-        assertTrue(isSorted(t));
-    }
-
-    @org.junit.jupiter.api.Test
-    void mergeSortTest6() {
-        var copy = Data.test5;
-        var t = Arrays.copyOf(copy, copy.length);
-        Sort.mergeSort(t);
-        assertTrue(isSorted(t));
-    }
-
-    @org.junit.jupiter.api.Test
+    @Test
     void heapSort1() {
-        var c = Data.data;
+        var c = test1;
         var t = Arrays.copyOf(c, c.length);
         Sort.heapSort(t);
         assertTrue(isSorted(t));
     }
 
-    @org.junit.jupiter.api.Test
+    @Test
     void heapSort2() {
-        var c = Data.test1;
+        var c = test2;
         var t = Arrays.copyOf(c, c.length);
         Sort.heapSort(t);
         assertTrue(isSorted(t));
     }
 
-    @org.junit.jupiter.api.Test
+    @Test
     void heapSort3() {
-        var c = Data.test2;
+        var c = test3;
         var t = Arrays.copyOf(c, c.length);
         Sort.heapSort(t);
         assertTrue(isSorted(t));
     }
 
-    @org.junit.jupiter.api.Test
-    void heapSort4() {
-        var c = Data.test3;
-        var t = Arrays.copyOf(c, c.length);
-        Sort.heapSort(t);
-        assertTrue(isSorted(t));
-    }
-
-    @org.junit.jupiter.api.Test
-    void heapSort5() {
-        var c = Data.test4;
-        var t = Arrays.copyOf(c, c.length);
-        Sort.heapSort(t);
-        assertTrue(isSorted(t));
-    }
-
-    @org.junit.jupiter.api.Test
-    void heapSort6() {
-        var c = Data.test5;
-        var t = Arrays.copyOf(c, c.length);
-        Sort.heapSort(t);
-        assertTrue(isSorted(t));
-    }
-
-    @org.junit.jupiter.api.Test
+    @Test
     void quickSort1() {
-        var c = Data.data;
+        var c = test1;
         var t = Arrays.copyOf(c, c.length);
         Sort.quickSort(t);
         assertTrue(isSorted(t));
     }
 
-    @org.junit.jupiter.api.Test
+    @Test
     void quickSort2() {
-        var c = Data.test1;
+        var c = test2;
         var t = Arrays.copyOf(c, c.length);
         Sort.quickSort(t);
         assertTrue(isSorted(t));
     }
 
-    @org.junit.jupiter.api.Test
+    @Test
     void quickSort3() {
-        var c = Data.test2;
+        var c = test3;
         var t = Arrays.copyOf(c, c.length);
         Sort.quickSort(t);
         assertTrue(isSorted(t));
     }
 
-    @org.junit.jupiter.api.Test
-    void quickSort4() {
-        var c = Data.test3;
-        var t = Arrays.copyOf(c, c.length);
-        Sort.quickSort(t);
-        assertTrue(isSorted(t));
-    }
-
-    @org.junit.jupiter.api.Test
-    void quickSort5() {
-        var c = Data.test4;
-        var t = Arrays.copyOf(c, c.length);
-        Sort.quickSort(t);
-        assertTrue(isSorted(t));
-    }
-
-    @org.junit.jupiter.api.Test
-    void quickSort6() {
-        var c = Data.test5;
-        var t = Arrays.copyOf(c, c.length);
-        Sort.quickSort(t);
-        assertTrue(isSorted(t));
-    }
-
-    @org.junit.jupiter.api.Test
+    @Test
     void randQuickSort1() {
-        var c = Data.data;
+        var c = test1;
         var t = Arrays.copyOf(c, c.length);
         Sort.randQuickSort(t);
         assertTrue(isSorted(t));
     }
 
-    @org.junit.jupiter.api.Test
+    @Test
     void randQuickSort2() {
-        var c = Data.test1;
+        var c = test2;
         var t = Arrays.copyOf(c, c.length);
         Sort.randQuickSort(t);
         assertTrue(isSorted(t));
     }
 
-    @org.junit.jupiter.api.Test
+    @Test
     void randQuickSort3() {
-        var c = Data.test2;
+        var c = test3;
         var t = Arrays.copyOf(c, c.length);
         Sort.randQuickSort(t);
         assertTrue(isSorted(t));
     }
 
-    @org.junit.jupiter.api.Test
-    void randQuickSort4() {
-        var c = Data.test3;
-        var t = Arrays.copyOf(c, c.length);
-        Sort.randQuickSort(t);
-        assertTrue(isSorted(t));
-    }
 
-    @org.junit.jupiter.api.Test
-    void randQuickSort5() {
-        var c = Data.test4;
-        var t = Arrays.copyOf(c, c.length);
-        Sort.randQuickSort(t);
-        assertTrue(isSorted(t));
-    }
-
-    @org.junit.jupiter.api.Test
-    void randQuickSort6() {
-        var c = Data.test5;
-        var t = Arrays.copyOf(c, c.length);
-        Sort.randQuickSort(t);
-        assertTrue(isSorted(t));
-        assertFalse(isSorted(c));
-    }
-
-    @org.junit.jupiter.api.Test
+    @Test
     void countingSort1() {
-        var c = Data.data;
+        var c = test4;
         var t = Arrays.copyOf(c, c.length);
         Sort.countingSort(t);
         assertTrue(isSorted(t));
     }
 
-    @org.junit.jupiter.api.Test
+    @Test
     void countingSort2() {
-        var c = Data.test1;
+        var c = test5;
         var t = Arrays.copyOf(c, c.length);
         Sort.countingSort(t);
         assertTrue(isSorted(t));
     }
 
-    @org.junit.jupiter.api.Test
+    @Test
     void countingSort3() {
-        var c = Data.test2;
+        var c = test6;
         var t = Arrays.copyOf(c, c.length);
         Sort.countingSort(t);
         assertTrue(isSorted(t));
     }
 
-    @org.junit.jupiter.api.Test
-    void countingSort4() {
-        var c = Data.test3;
-        var t = Arrays.copyOf(c, c.length);
-        Sort.countingSort(t);
-        assertTrue(isSorted(t));
-    }
-
-    @org.junit.jupiter.api.Test
-    void countingSort5() {
-        var c = Data.test4;
-        var t = Arrays.copyOf(c, c.length);
-        Sort.countingSort(t);
-        assertTrue(isSorted(t));
-    }
-
-    @org.junit.jupiter.api.Test
-    void countingSort6() {
-        var c = Data.test5;
-        var t = Arrays.copyOf(c, c.length);
-        Sort.countingSort(t);
-        assertTrue(isSorted(t));
-    }
-
-    @org.junit.jupiter.api.Test
+    @Test
     void radixSort1() {
-        SimpleDate[] t = Data.buildDate();
+        SimpleDate[] t = buildDate();
         Sort.radixSort(t);
         assertTrue(isSorted(t));
     }
 
-    @org.junit.jupiter.api.Test
+    @Test
     void bucketSort1() {
-        var t = Data.bucket_test;
+        var t = bucket_test;
         Sort.bucketSort(t);
         assertTrue(isSorted(t));
     }
 
-    @org.junit.jupiter.api.Test
+    @Test
     void bucketSort2() {
-        var t = Data.bucket_test1;
+        var t = bucket_test1;
         Sort.bucketSort(t);
         assertTrue(isSorted(t));
     }
 
-    @org.junit.jupiter.api.Test
+    @Test
     void bucketSort3() {
-        var t = Data.bucket_test2;
+        var t = bucket_test2;
         Sort.bucketSort(t);
         assertTrue(isSorted(t));
     }
