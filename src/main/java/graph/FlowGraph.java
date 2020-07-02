f8188224c4856ab6e50dcad2afd02cce96acfb98
nathan <1136026744@qq.com>
2020-07-02 17:21:29 +0800

    add class

--- src/main/java/graph/FlowGraph.java
+++ src/main/java/graph/FlowGraph.java
@@ -42,6 +42,41 @@ public class FlowGraph<V> {
         @Override
         public int hashCode(){ return Objects.hash(former_vertex, later_vertex, capacity); }
     }
+    public static class ResidualFlowEdge<T> {
+        private final T former_vertex;
+        private final T later_vertex;
+        double flow;
+
+        ResidualFlowEdge(T former, T later, double flow){
+            Objects.requireNonNull(former);
+            Objects.requireNonNull(later);
+            former_vertex = former;
+            later_vertex = later;
+            this.flow = flow;
+        }
+
+        @Override
+        public boolean equals(Object other_edge) {
+            if (other_edge == this) return true;
+            if (!(other_edge instanceof ResidualFlowEdge)) return false;
+            return former_vertex.equals(((ResidualFlowEdge<?>) other_edge).former_vertex) &&
+                    later_vertex.equals((((ResidualFlowEdge<?>) other_edge).later_vertex));
+        }
+
+        public T getFormerVertex() { return former_vertex; }
+
+        public T getLaterVertex() { return later_vertex; }
+
+        public double getFlow() { return flow; }
+
+        @Override
+        public String toString(){
+            return String.format("[ResidualFlowEdge(%s >>> %s)], flow:%f", former_vertex, later_vertex,flow);
+        }
+
+        @Override
+        public int hashCode(){ return Objects.hash(former_vertex, later_vertex); }
+    }
     private final Map<V, List<FlowEdge<V>>> edges_map = new HashMap<>();
 
     public FlowGraph(List<V> vertices){
@@ -59,5 +94,34 @@ public class FlowGraph<V> {
         edges_list.add(new FlowEdge<>(vertex,neighbor,capacity));
     }
 
-    public List<FlowEdge<V>> getFlowEdges(V vertex){ return new ArrayList<>(edges_map.get(vertex)); }
+    public List<FlowEdge<V>> getNeighborEdges(V vertex){ return new ArrayList<>(edges_map.get(vertex)); }
+
+    public double getResidualCapacity(V former, V later){
+        var o1 = tryGetFlowEdge(former,later);
+        if(o1.isPresent()){
+            var e = o1.get();
+            return e.capacity - e.flow;
+        }else{
+            var o2 = tryGetFlowEdge(later,former);
+            return o2.map(edge -> edge.flow).orElse(0.0);
+        }
+    }
+
+    public Optional<FlowEdge<V>> tryGetFlowEdge(V vertex, V another){
+        var edges_list = edges_map.get(vertex);
+        for(var e : edges_list){
+            if(e.later_vertex.equals(another)){
+                return Optional.of(e);
+            }
+        }
+        return Optional.empty();
+    }
+
+    public double getCapacity(V u, V v){
+        var o = tryGetFlowEdge(u,v);
+        if(o.isPresent()){
+            return o.get().capacity;
+        };
+        throw new NoSuchElementException();
+    }
 }
--- src/main/java/graph/MaxFlow.java
+++ src/main/java/graph/MaxFlow.java
@@ -1,4 +1,48 @@
 package graph;
 
+import java.util.Objects;
+
 public class MaxFlow {
+    public static class FlowVertex<V>{
+        private final V content;
+        private double e; // excess
+        private int h; // height
+        private final int hash;
+
+        public FlowVertex(V content){
+            Objects.requireNonNull(content);
+            this.content = content;
+            hash = content.hashCode();
+        }
+
+        @Override
+        public boolean equals(Object other){
+            if(other == this) return true;
+            if(!(other instanceof FlowVertex)) return false;
+            return ((FlowVertex<?>) other).content.equals(content);
+        }
+
+        @Override
+        public int hashCode(){ return hash; }
+
+        @Override
+        public String toString() { return String.format("FlowVertex: %s",content.toString()); }
+    }
+    private static <T> void push(FlowVertex<T> u, FlowVertex<T> v, FlowGraph<FlowVertex<T>> graph){
+        var delta = Math.min(u.e, graph.getResidualCapacity(u,v));
+        var o1 = graph.tryGetFlowEdge(u,v);
+        if(o1.isPresent()){
+            var e = o1.get();
+            e.flow += delta;
+        }else{
+            var o2 = graph.tryGetFlowEdge(v,u);
+            if(o2.isPresent()){
+                o2.get().flow -= delta;
+            }else{
+                throw new AssertionError();
+            }
+        }
+        u.e -= delta;
+        v.e += delta;
+    }
 }
