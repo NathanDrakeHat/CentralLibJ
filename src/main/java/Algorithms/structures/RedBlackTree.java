c83ff6c7f9da1a12dc42483da95adc4b6098f7d6
nathan <1136026744@qq.com>
2020-08-26 20:58:58 +0800

    add iterator in RedBlackTree.java

--- src/main/java/Algorithms/structures/RedBlackTree.java
+++ src/main/java/Algorithms/structures/RedBlackTree.java
@@ -1,12 +1,14 @@
 package Algorithms.structures;
 
-import java.util.Comparator;
-import java.util.NoSuchElementException;
-import java.util.Objects;
+import Algorithms.tools.Pair;
+
+import java.util.*;
 import java.util.function.BiConsumer;
 
 
-public final class RedBlackTree<K, V>{
+public final class RedBlackTree<K, V> implements Iterable<Pair<K, V>> {
+
+
     static final class ColorNode<P, Q> {
         P key;
         Q value;
@@ -43,51 +45,89 @@ public final class RedBlackTree<K, V>{
         }
     }
 
+    private final class StackIterator implements Iterator<Pair<K, V>> {
+        private ColorNode<K, V> ptr;
+        private final Deque<ColorNode<K, V>> stack = new LinkedList<>();
+        private boolean poppedBefore = false;
+        private boolean finish = false;
+
+        public StackIterator() {
+            ptr = root;
+            if (ptr == sentinel) finish = true;
+        }
+
+        @Override
+        public boolean hasNext() {
+            return !finish && ptr != null;
+        }
+
+        @Override
+        public Pair<K, V> next() {
+            while (ptr != null) {
+                if (ptr.left != sentinel && !poppedBefore) // if popped before, walk to right
+                {
+                    stack.push(ptr);
+                    ptr = ptr.left;
+                } else {
+                    var t = ptr;
+                    if (ptr.right != sentinel) {
+                        ptr = ptr.right;
+                        poppedBefore = false;
+                    } else {
+                        if (stack.size() != 0) {
+                            ptr = stack.pop();
+                            poppedBefore = true;
+                        } else ptr = null;
+                    }
+                    return new Pair<>(t.key, t.value);
+                }
+            }
+            finish = true;
+            throw new IllegalStateException("Iterate finish.");
+        }
+    }
+
     enum COLOR {RED, BLACK}
 
     ColorNode<K, V> root;
     private final Comparator<K> k_comparator;
     private final ColorNode<K, V> sentinel = new ColorNode<>(COLOR.BLACK);// sentinel: denote leaf and parent of root
-    
+
 
     public RedBlackTree(Comparator<K> k_comparator) {
         Objects.requireNonNull(k_comparator);
         this.k_comparator = k_comparator;
         root = sentinel;
     }
-    
+
     public V getValueOfMinKey() {
         if (root != null && sentinel != root) {
             return getMinimum(root).value;
-        }
-        else {
+        } else {
             throw new NoSuchElementException("null tree");
         }
     }
-    
+
     public K getMinKey() {
         if (root != null && sentinel != root) {
             return getMinimum(root).key;
-        }
-        else {
+        } else {
             throw new NoSuchElementException("null tree");
         }
     }
-    
+
     public V getValueOfMaxKey() {
         if (root != null && root != sentinel) {
             return getMaximum(root).value;
-        }
-        else {
+        } else {
             throw new NoSuchElementException("null tree");
         }
     }
-    
+
     public K getMaxKey() {
         if (root != null && root != sentinel) {
             return getMaximum(root).key;
-        }
-        else {
+        } else {
             throw new NoSuchElementException("null tree");
         }
     }
@@ -123,14 +163,11 @@ public final class RedBlackTree<K, V>{
     private int getCount(ColorNode<K, V> n) { //overload trick
         if (n.right != sentinel && n.left == sentinel) {
             return getCount(n.right) + 1;
-        }
-        else if (n.right == sentinel && n.left != sentinel) {
+        } else if (n.right == sentinel && n.left != sentinel) {
             return getCount(n.left) + 1;
-        }
-        else if (n.right != sentinel) {
+        } else if (n.right != sentinel) {
             return getCount(n.left) + getCount(n.right) + 1;
-        }
-        else {
+        } else {
             return 1;
         }
     }
@@ -167,24 +204,21 @@ public final class RedBlackTree<K, V>{
             resetRoot(n);
             root.right = sentinel;
             root.left = sentinel;
-        }
-        else {
+        } else {
             var store = sentinel;
             var ptr = root;
             while (ptr != sentinel) {
                 store = ptr;
                 if (k_comparator.compare(n.key, ptr.key) < 0) {
                     ptr = ptr.left;
-                }
-                else {
+                } else {
                     ptr = ptr.right;
                 }
             }
             n.parent = store;
             if (k_comparator.compare(n.key, store.key) < 0) {
                 store.left = n;
-            }
-            else {
+            } else {
                 store.right = n;
             }
             n.left = sentinel;
@@ -203,8 +237,7 @@ public final class RedBlackTree<K, V>{
                     ptr.parent.parent.setRed();
                     ptr = ptr.parent.parent;
                     continue;
-                }
-                else if (ptr == ptr.parent.right) { //case 2 convert to case 3
+                } else if (ptr == ptr.parent.right) { //case 2 convert to case 3
                     ptr = ptr.parent;
                     leftRotate(ptr);
                 }
@@ -212,8 +245,7 @@ public final class RedBlackTree<K, V>{
                 ptr.parent.parent.setRed();
                 rightRotate(ptr.parent.parent); // ptr.getParent will be black and then break
                 ptr = ptr.parent;
-            }
-            else {
+            } else {
                 var left = ptr.parent.parent.left;
                 if (left.isRed()) {
                     ptr.parent.setBlack();
@@ -221,8 +253,7 @@ public final class RedBlackTree<K, V>{
                     ptr.parent.parent.setRed();
                     ptr = ptr.parent.parent;
                     continue;
-                }
-                else if (ptr == ptr.parent.left) {
+                } else if (ptr == ptr.parent.left) {
                     ptr = ptr.parent;
                     rightRotate(ptr);
                 }
@@ -250,19 +281,16 @@ public final class RedBlackTree<K, V>{
         if (ptr.left == sentinel) {
             fix_up = target.right;
             transplant(target, fix_up);
-        }
-        else if (ptr.right == sentinel) {
+        } else if (ptr.right == sentinel) {
             fix_up = target.left;
             transplant(target, fix_up);
-        }
-        else {
+        } else {
             ptr = getSuccessor(target);
             ptr_color = ptr.color;
             fix_up = ptr.right;
             if (ptr.parent == target) {
                 fix_up.parent = ptr; // in case of sentinel refer to target
-            }
-            else {
+            } else {
                 transplant(ptr, ptr.right);
                 ptr.right = target.right;
                 target.right.parent = ptr;
@@ -292,8 +320,7 @@ public final class RedBlackTree<K, V>{
                     sibling.setRed();
                     fix_up = fix_up.parent;
                     continue;
-                }
-                else if (sibling.right.isBlack()) { // case3: sibling left red, right black. convert case4
+                } else if (sibling.right.isBlack()) { // case3: sibling left red, right black. convert case4
                     sibling.left.setBlack();
                     sibling.setRed();
                     rightRotate(sibling);
@@ -303,8 +330,7 @@ public final class RedBlackTree<K, V>{
                 fix_up.parent.setBlack();
                 sibling.right.setBlack();
                 leftRotate(fix_up.parent);
-            }
-            else {
+            } else {
                 sibling = fix_up.parent.left;
                 if (sibling.isRed()) {
                     sibling.setBlack();
@@ -316,8 +342,7 @@ public final class RedBlackTree<K, V>{
                     sibling.setRed();
                     fix_up = fix_up.parent;
                     continue;
-                }
-                else if (sibling.left.isBlack()) {
+                } else if (sibling.left.isBlack()) {
                     sibling.right.setBlack();
                     sibling.setRed();
                     leftRotate(sibling);
@@ -336,17 +361,15 @@ public final class RedBlackTree<K, V>{
     private void transplant(ColorNode<K, V> a, ColorNode<K, V> b) {
         if (a.parent == sentinel) {
             resetRoot(b);
-        }
-        else if (a.parent.right == a) {
+        } else if (a.parent.right == a) {
             a.parent.right = b;
             b.parent = a.parent; // permissible if b is sentinel
-        }
-        else {
+        } else {
             a.parent.left = b;
             b.parent = a.parent;
         }
     }
-    
+
     public V search(K key) {
         if (root == sentinel) {
             throw new NoSuchElementException();
@@ -358,11 +381,9 @@ public final class RedBlackTree<K, V>{
     private ColorNode<K, V> search(ColorNode<K, V> n, K key) {
         if (k_comparator.compare(n.key, key) == 0) {
             return n;
-        }
-        else if (k_comparator.compare(n.key, key) > 0 && n.left != sentinel) {
+        } else if (k_comparator.compare(n.key, key) > 0 && n.left != sentinel) {
             return search(n.left, key);
-        }
-        else if (k_comparator.compare(n.key, key) < 0 && n.right != sentinel) {
+        } else if (k_comparator.compare(n.key, key) < 0 && n.right != sentinel) {
             return search(n.right, key);
         }
         throw new NoSuchElementException();
@@ -379,11 +400,9 @@ public final class RedBlackTree<K, V>{
         right_node.parent = left_node.parent; // double link right_node to left_node parent
         if (left_node.parent == sentinel) {
             resetRoot(right_node);
-        }
-        else if (left_node.parent.left == left_node) {
+        } else if (left_node.parent.left == left_node) {
             left_node.parent.left = right_node;
-        }
-        else {
+        } else {
             left_node.parent.right = right_node;
         }
         //exchange
@@ -402,11 +421,9 @@ public final class RedBlackTree<K, V>{
         left_node.parent = right_node.parent; // double link right_node to left_node parent
         if (right_node.parent == sentinel) {
             resetRoot(left_node);
-        }
-        else if (right_node.parent.right == right_node) {
+        } else if (right_node.parent.right == right_node) {
             right_node.parent.right = left_node;
-        }
-        else {
+        } else {
             right_node.parent.left = left_node;
         }
         //exchange
@@ -443,8 +460,7 @@ public final class RedBlackTree<K, V>{
     private ColorNode<K, V> getSuccessor(ColorNode<K, V> current) {
         if (current.right != sentinel) {
             return getMinimum(current.right);
-        }
-        else {
+        } else {
             var target = current.parent;
             var target_right = current;
             while ((target != sentinel) && target.right == target_right) {
@@ -459,8 +475,7 @@ public final class RedBlackTree<K, V>{
     private ColorNode<K, V> getPredecessor(ColorNode<K, V> current) {
         if (current.left != sentinel) {
             return getMaximum(current.left);
-        }
-        else {
+        } else {
             var target = current.parent;
             var target_left = current;
             while ((target != sentinel) && (target.left == target_left)) {
@@ -470,5 +485,10 @@ public final class RedBlackTree<K, V>{
             }
             return target;
         }
+    }
+
+    @Override
+    public Iterator<Pair<K, V>> iterator() {
+        return new StackIterator();
     }
 }
---
+++ src/main/java/Algorithms/tools/Pair.java
@@ -0,0 +1,11 @@
+package Algorithms.tools;
+
+public class Pair<I1, I2> {
+    public final I1 first;
+    public final I2 second;
+
+    public Pair(I1 a, I2 b){
+        first = a;
+        second= b;
+    }
+}
