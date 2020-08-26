7c0d960bc4c7de5c5cb5423367f47ab68ab063ea
nathan <1136026744@qq.com>
2020-08-26 21:26:21 +0800

    add

---
+++ src/main/java/Algorithms/tools/Quadruple.java
@@ -0,0 +1,15 @@
+package Algorithms.tools;
+
+public class Quadruple<I1,I2,I3,I4> {
+    public final I1 first;
+    public final I2 second;
+    public final I3 third;
+    public final I4 fourth;
+
+    public Quadruple(I1 a, I2 b, I3 c, I4 d){
+        first = a;
+        second = b;
+        third = c;
+        fourth = d;
+    }
+}
