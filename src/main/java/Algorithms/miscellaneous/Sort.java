b817071441af2073f31e85d3a35ab373d2b278f1
nathan <1136026744@qq.com>
2020-09-06 11:58:22 +0800

    remove unused class

--- src/main/java/Algorithms/miscellaneous/Sort.java
+++ src/main/java/Algorithms/miscellaneous/Sort.java
@@ -231,32 +231,4 @@ public final class Sort {
         }
     }
 
-    private static class SingleLinkedNode<V> {
-        private V val;
-        private SingleLinkedNode<V> parent;
-
-        public SingleLinkedNode() {
-        }
-
-        public SingleLinkedNode(V val) {
-            this.val = val;
-            this.parent = null;
-        }
-
-        public SingleLinkedNode<V> getParent() {
-            return this.parent;
-        }
-
-        public void setParent(SingleLinkedNode<V> p) {
-            this.parent = p;
-        }
-
-        public V getContent() {
-            return this.val;
-        }
-
-        public void setContent(V val) {
-            this.val = val;
-        }
-    }
 }
