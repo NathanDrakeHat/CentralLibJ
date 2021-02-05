534fbe4dddffe99d12dc5922302f89fcfd6a97ed
nathan <1136026744@qq.com>
2021-02-05 19:39:56 +0800

    reformat

--- src/main/java/org/nathan/AlgorithmsJava/tools/IntegerRange.java
+++ src/main/java/org/nathan/AlgorithmsJava/tools/IntegerRange.java
@@ -7,7 +7,8 @@ import java.util.Iterator;
 public class IntegerRange implements Iterable<Integer> {
     private final int high;
     private int index;
-    public IntegerRange(int l, int h){
+
+    public IntegerRange(int l, int h) {
         high = h;
         index = l;
     }
@@ -15,10 +16,10 @@ public class IntegerRange implements Iterable<Integer> {
     @NotNull
     @Override
     public Iterator<Integer> iterator() {
-        return new Iterator<Integer>() {
+        return new Iterator<>() {
             @Override
             public boolean hasNext() {
-                return index<high;
+                return index < high;
             }
 
             @Override
