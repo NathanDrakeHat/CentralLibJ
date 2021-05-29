ed73eb857a3d96f2ee85912a0a829c014eb073e7
NathanDrakeHat <1136026744@qq.com>
2021-05-29 11:37:54 +0800

    add doc

--- src/main/java/org/nathan/Main.java
+++ src/main/java/org/nathan/Main.java
@@ -3,19 +3,23 @@ package org.nathan;
 import org.nathan.acm.ACM;
 import org.nathan.centralUtils.utils.IOUtils;
 
-import java.util.BitSet;
-import java.util.HashSet;
-import java.util.Set;
-import java.util.WeakHashMap;
+import java.util.*;
 
 import static org.nathan.centralUtils.utils.IOUtils.*;
+import static org.nathan.centralUtils.utils.ArrayUtils.*;
 
 
 class Main{
-    public static void main(String[] args){
+    public static void gitPush(String[] args){
         if(args.length == 1){
             var i = Integer.valueOf(args[0]);
             IOUtils.system(String.format("git_proxy \"git push\" %s", args[0]));
         }
     }
+
+    public static void main(String[] args){
+        gitPush(args);
+    }
+
+
 }
