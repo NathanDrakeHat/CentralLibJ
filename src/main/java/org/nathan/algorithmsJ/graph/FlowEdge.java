0c9e57f62d11466b69b1c4d2976a50006c8cc32d
NathanDrakeHat <1136026744@qq.com>
2021-06-10 22:17:18 +0800

    save flow edge

--- src/main/java/org/nathan/algorithmsJ/graph/FlowEdge.java
+++ src/main/java/org/nathan/algorithmsJ/graph/FlowEdge.java
@@ -6,8 +6,10 @@ public class FlowEdge<Id> extends BaseEdge<Id>{
   int capacity;
   int flow;
 
-  public FlowEdge(@NotNull Id f, @NotNull Id l){
+  public FlowEdge(@NotNull Id f, @NotNull Id l, int capacity, int flow){
     super(f, l);
+    this.capacity = capacity;
+    this.flow = flow;
   }
 
 }
