0a24aa794728767c5c2b4061064752a0cec2e760
NathanDrakeHat <1136026744@qq.com>
2021-06-13 16:27:04 +0800

    add knuth shuffle

--- src/main/java/org/nathan/algorithmsJ/misc/Shuffle.java
+++ src/main/java/org/nathan/algorithmsJ/misc/Shuffle.java
@@ -1,5 +1,16 @@
 package org.nathan.algorithmsJ.misc;
 
+import java.util.SplittableRandom;
+
 // TODO implement shuffle
 public class Shuffle{
+  public static void KnuthShuffle(double[] array){
+    var rand = new SplittableRandom();
+    for(int i = 1; i < array.length; i++){
+      int r = rand.nextInt(i + 1);
+      var t = array[i];
+      array[i] = array[r];
+      array[r] = t;
+    }
+  }
 }
