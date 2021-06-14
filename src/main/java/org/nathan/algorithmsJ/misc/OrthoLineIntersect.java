bf457281c88a6259bac9de2c48549c61a3c7acc1
NathanDrakeHat <1136026744@qq.com>
2021-06-14 19:38:55 +0800

    pass test

--- src/main/java/org/nathan/algorithmsJ/misc/OrthoLineIntersect.java
+++ src/main/java/org/nathan/algorithmsJ/misc/OrthoLineIntersect.java
@@ -21,12 +21,12 @@ public class OrthoLineIntersect{
     Map<P, L> pointsToLine = new HashMap<>();
     List<P> scanPoints = new ArrayList<>();
     Set<P> hPoints = new HashSet<>();
-    Map<P, P> otherPoint = new HashMap<>();
+    Map<P, P> otherEndPoint = new HashMap<>();
 
     for(var line : lines){
       var point_pair = toPoints.apply(line);
-      otherPoint.put(point_pair.first(), point_pair.second());
-      otherPoint.put(point_pair.second(), point_pair.first());
+      otherEndPoint.put(point_pair.first(), point_pair.second());
+      otherEndPoint.put(point_pair.second(), point_pair.first());
 
       var x1 = getX.applyAsDouble(point_pair.first());
       var x2 = getX.applyAsDouble(point_pair.second());
@@ -50,7 +50,7 @@ public class OrthoLineIntersect{
     var funcAddIntersect = new Object(){
       void apply(P v_p){
         var vl = pointsToLine.get(v_p);
-        var ap = otherPoint.get(v_p);
+        var ap = otherEndPoint.get(v_p);
         var y1 = getY.applyAsDouble(ap);
         var y2 = getY.applyAsDouble(v_p);
         var hls = HYToHL_tree.keyRangeSearch(Math.min(y1, y2), Math.max(y1, y2));
@@ -62,7 +62,7 @@ public class OrthoLineIntersect{
 
     var funcRmFromTree = new Object(){
       void apply(P h_p){
-        inTree.remove(otherPoint.get(h_p));
+        inTree.remove(otherEndPoint.get(h_p));
         HYToHL_tree.delete(getY.applyAsDouble(h_p));
       }
     };
@@ -71,7 +71,7 @@ public class OrthoLineIntersect{
       var p = scanPoints.get(i);
 
       if(hPoints.contains(p)){
-        if(inTree.contains(otherPoint.get(p))){
+        if(inTree.contains(otherEndPoint.get(p))){
           if(i + 1 < scanPoints.size() && getX.applyAsDouble(p) == getX.applyAsDouble(scanPoints.get(i+1))){
             var ti = i + 1;
             while(getX.applyAsDouble(p) == getX.applyAsDouble(scanPoints.get(ti)) && hPoints.contains(scanPoints.get(ti))){
@@ -84,7 +84,7 @@ public class OrthoLineIntersect{
               funcAddIntersect.apply(p);
             }
             else {
-              funcRmFromTree.apply(otherPoint.get(p));
+              funcRmFromTree.apply(otherEndPoint.get(p));
               var np = scanPoints.get(i+1);
               do{
                 funcRmFromTree.apply(np);
@@ -96,7 +96,7 @@ public class OrthoLineIntersect{
           }
           else{
             // remove last
-            funcRmFromTree.apply(otherPoint.get(p));
+            funcRmFromTree.apply(otherEndPoint.get(p));
           }
         }
         else{
