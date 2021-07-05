36cbdb1e7ed47826c49e4be7203842715e7ba02c
NathanDrakeHat <1136026744@qq.com>
2021-07-05 20:43:40 +0800

    add new acm

--- src/main/java/org/nathan/acm/ACM0x10.java
+++ src/main/java/org/nathan/acm/ACM0x10.java
@@ -31,12 +31,41 @@ public class ACM0x10 {
     }
     {
       int acc_width = 0;
-      while (!rectRecord.isEmpty()){
+      while (!rectRecord.isEmpty()) {
         var r = rectRecord.removeLast();
         acc_width += r.first();
         max_area = Math.max(max_area, r.second() * acc_width);
       }
     }
     return max_area;
+  }
+
+  /**
+   * subarray of max sum
+   *
+   * @param array array
+   * @param M     max length of subarray
+   * @return subarray
+   */
+  public static double monotonousQueue(double[] array, int M) {
+    double[] s = new double[array.length + 1];
+    System.arraycopy(array, 0, s, 1, array.length);
+    for (int i = 2; i < s.length; i++) {
+      s[i] += s[i - 1];
+    }
+    Deque<Integer> deque = new ArrayDeque<>();
+    deque.addFirst(0);
+    double ans = Double.NEGATIVE_INFINITY;
+    for (int i = 1; i <= array.length; i++) {
+      while (!deque.isEmpty() && i - deque.getFirst() > M) {
+        deque.removeFirst();
+      }
+      ans = Math.max(ans, s[i] - s[deque.getFirst()]);
+      while (!deque.isEmpty() && s[deque.getLast()] >= s[i]) {
+        deque.removeLast();
+      }
+      deque.addLast(i);
+    }
+    return ans;
   }
 }
--- src/test/java/org/nathan/acm/ACM0x10Test.java
+++ src/test/java/org/nathan/acm/ACM0x10Test.java
@@ -2,29 +2,67 @@ package org.nathan.acm;
 
 import org.junit.jupiter.api.Test;
 import org.nathan.centralUtils.tuples.Tuple;
+import org.nathan.centralUtils.utils.ArrayUtils;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.SplittableRandom;
 
-import static org.junit.jupiter.api.Assertions.*;
-import static org.nathan.acm.ACM0x10.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.nathan.acm.ACM0x10.largestRectInHist;
+import static org.nathan.acm.ACM0x10.monotonousQueue;
 
 class ACM0x10Test {
+  final int iteration = 20;
 
   List<Tuple<Integer, Integer>> LRITCases = new ArrayList<>(7);
 
   {
-    LRITCases.add(new Tuple<>(1,2));
-    LRITCases.add(new Tuple<>(1,1));
-    LRITCases.add(new Tuple<>(1,4));
-    LRITCases.add(new Tuple<>(1,5));
-    LRITCases.add(new Tuple<>(1,1));
-    LRITCases.add(new Tuple<>(1,3));
-    LRITCases.add(new Tuple<>(1,3));
+    LRITCases.add(new Tuple<>(1, 2));
+    LRITCases.add(new Tuple<>(1, 1));
+    LRITCases.add(new Tuple<>(1, 4));
+    LRITCases.add(new Tuple<>(1, 5));
+    LRITCases.add(new Tuple<>(1, 1));
+    LRITCases.add(new Tuple<>(1, 3));
+    LRITCases.add(new Tuple<>(1, 3));
   }
 
   @Test
   void largestRectInHistTest() {
     assertEquals(8, largestRectInHist(LRITCases));
+  }
+
+  double[][] mqCases;
+  double[] mqAnswers;
+
+  {
+    mqCases = new double[iteration][];
+    mqAnswers = new double[iteration];
+    var rand = new SplittableRandom();
+    for (int i = 0; i < iteration; i++) {
+      int len = rand.nextInt(3, 6);
+      mqCases[i] = ArrayUtils.randomDoubleArray(-5, 10, len);
+      int m = len / 2;
+      double[] temp = new double[mqCases[i].length + 1];
+      System.arraycopy(mqCases[i], 0, temp, 1, temp.length-1);
+      for (int t = 2; t < temp.length; t++) {
+        temp[t] += temp[t - 1];
+      }
+      double ans = Double.NEGATIVE_INFINITY;
+      for (int j = 0; j < temp.length; j++) {
+        for (int k = j + 1; k < temp.length && k - j <= m; k++) {
+          ans = Math.max(ans, temp[k] - temp[j]);
+        }
+      }
+      mqAnswers[i] = ans;
+    }
+  }
+
+  @Test
+  void monotonousQueueTest() {
+    for (int i = 0; i < iteration; i++) {
+      var ans = monotonousQueue(mqCases[i], mqCases[i].length / 2);
+      assertEquals(mqAnswers[i], ans);
+    }
   }
 }
