f093b1cded874964c6dbcb8a8ca4dec8ff0973a7
NathanDrakeHat <1136026744@qq.com>
2021-07-20 20:40:16 +0800

    add new acm

--- src/test/java/org/nathan/acm/ACM0x30Test.java
+++ src/test/java/org/nathan/acm/ACM0x30Test.java
@@ -2,7 +2,6 @@ package org.nathan.acm;
 
 import it.unimi.dsi.fastutil.ints.IntList;
 import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.*;
 import org.nathan.algsJ.numeric.NumberTheory;
 import org.nathan.centralUtils.tuples.Tuple;
 
@@ -10,15 +9,17 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.SplittableRandom;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 class ACM0x30Test{
-  static final int iteration = 1;
+  static final int iteration = 100;
 
   List<Tuple<Integer, Integer>> pdCases;
   IntList primes;
   int[] pdAnswers = new int[iteration];
 
   {
-    primes = NumberTheory.primesGenEulerSieve((int) Math.pow(10,8));
+    primes = NumberTheory.primesGenEulerSieve((int) Math.pow(10, 4));
     pdCases = new ArrayList<>(iteration);
     var rand = new SplittableRandom();
     for(int i = 0; i < iteration; i++){
@@ -26,11 +27,12 @@ class ACM0x30Test{
       var e = rand.nextInt(primes.size() / 2, primes.size());
       pdCases.add(new Tuple<>(s, e));
       int max = -1;
-      for(int j = s; j < e - 1; j++){
+      for(int j = s; j < e; j++){
         var a = primes.getInt(j);
         var b = primes.getInt(j + 1);
         max = Math.max(max, b - a);
       }
+      pdAnswers[i] = max;
     }
   }
 
@@ -41,7 +43,8 @@ class ACM0x30Test{
       var t = pdCases.get(i);
       var a = t.first();
       var b = t.second();
-//      assertEquals(pdAnswers[i], ACM0x30.primeDistance(primes.getInt(a), primes.getInt(b)));
+      var ans = ACM0x30.primeDistance(primes.getInt(a), primes.getInt(b));
+      assertEquals(pdAnswers[i], ans);
     }
   }
 }
