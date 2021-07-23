f1afa31df62a8d2a11895abc043e691366d4dc7d
NathanDrakeHat <1136026744@qq.com>
2021-07-23 19:34:08 +0800

    change readme

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
-java implementation for part of 《Introduction to Algorithms》 and《The Algorithms 4th》coursera.
+utils class, java implementation for part of 《Introduction to Algorithms》, 《The Algorithms 4th》coursera and 《算法竞赛进阶指南 李煜东》.
 
 TODO:add links of pseudocode pages to code 
--- src/main/java/org/nathan/Main.java
+++ src/main/java/org/nathan/Main.java
@@ -4,18 +4,16 @@ package org.nathan;
 import org.nathan.centralib.utils.NativeUtils;
 
 
-
-
-class Main {
-  static class GitPush {
-    public static void gitPush(String[] args) {
+class Main{
+  static class GitPush{
+    public static void gitPush(String[] args){
       NativeUtils.GitProxy(args);
     }
   }
 
 
-  public static void main(String[] args) {
-    if (args.length > 0) {
+  public static void main(String[] args){
+    if(args.length > 0){
       GitPush.gitPush(args);
     }
   }
