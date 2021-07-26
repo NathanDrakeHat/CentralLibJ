867f2b4da4fc2df90e0e3827bdd5af2f3ab03617
NathanDrakeHat <1136026744@qq.com>
2021-07-26 15:13:39 +0800

    faster random

--- src/main/java/org/nathan/Main.java
+++ src/main/java/org/nathan/Main.java
@@ -4,7 +4,6 @@ package org.nathan;
 import it.unimi.dsi.util.XoRoShiRo128PlusRandom;
 import org.nathan.centralibj.utils.NativeUtils;
 
-// TODO faster random
 class Main{
   static class GitPush{
     public static void gitPush(String[] args){
