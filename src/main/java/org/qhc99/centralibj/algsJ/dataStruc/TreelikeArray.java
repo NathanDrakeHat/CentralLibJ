353f726ff88c83e2e5c96eab29dfbe9f486b8dd5
NathanDrakeHat <1136026744@qq.com>
2022-02-27 16:55:59 -0500

    save

---
+++ src/main/java/org/qhc99/centralibj/algsJ/dataStruc/TreelikeArray.java
@@ -0,0 +1,71 @@
+package org.qhc99.centralibj.algsJ.dataStruc;
+
+public class TreelikeArray {
+  final int[] arr;
+  public final int ArrayLength;
+
+  public TreelikeArray(int[] array) {
+    ArrayLength = array.length;
+    arr = new int[ArrayLength + 1];
+
+    for (int i = 1; i <= ArrayLength; i++) {
+      prefixSumAdd(i, array[i - 1]);
+    }
+  }
+
+  /**
+   * zero initialization
+   *
+   * @param N number
+   */
+  public TreelikeArray(int N) {
+    ArrayLength = N;
+    arr = new int[N + 1];
+  }
+
+  /**
+   * sum of i items
+   *
+   * @param count count
+   * @return suffix sum
+   */
+  public int prefixSumOf(int count) {
+    if (count < 0 || count >= arr.length) {
+      throw new IllegalArgumentException();
+    }
+    int ans = 0;
+    while (count != 0) {
+      ans += arr[count];
+      count -= lowBit(count);
+    }
+    return ans;
+  }
+
+  /**
+   * @param l inclusive
+   * @param h inclusive
+   * @return sum of range
+   */
+  public int prefixSumOfRange(int l, int h) {
+    if (l < 0 || l >= arr.length || h < 0 || h >= arr.length || h < l) {
+      throw new IllegalArgumentException();
+    }
+    return prefixSumOf(h) - prefixSumOf(l - 1);
+
+  }
+
+  public void prefixSumAdd(int idx, int diff) {
+    if (idx <= 0 || idx >= arr.length) {
+      throw new IllegalArgumentException();
+    }
+    int len = arr.length;
+    while (idx < len) {
+      arr[idx] += diff;
+      idx += lowBit(idx);
+    }
+  }
+
+  private static int lowBit(int x) {
+    return x & -x;
+  }
+}
