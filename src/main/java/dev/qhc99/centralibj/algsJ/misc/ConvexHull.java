05a2a78253ae45b4b2a3f94e29a430962edc0d50
qhc <onlinecqh99@gmail.com>
2022-11-05 11:28:11 -0400

    fix graham scan

--- src/main/java/dev/qhc99/centralibj/algsJ/misc/ConvexHull.java
+++ src/main/java/dev/qhc99/centralibj/algsJ/misc/ConvexHull.java
@@ -1,10 +1,13 @@
 package dev.qhc99.centralibj.algsJ.misc;
 
 
-import java.util.*;
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Deque;
+import java.util.List;
 import java.util.function.ToDoubleFunction;
 
-public class ConvexHull {
+public  class ConvexHull {
     public static <E> List<E> GrahamScan(
             List<E> points,
             ToDoubleFunction<E> getX,
@@ -12,59 +15,55 @@ public class ConvexHull {
         if (points.size() <= 3) {
             return points;
         }
-        List<E> ps = new ArrayList<>(points);
-        var start = ps.get(0);
-        for (var i = 0; i < ps.size(); i++) {
-            var p = ps.get(i);
+        var start = points.get(0);
+        int start_idx = 0;
+        for (var i = 0; i < points.size(); i++) {
+            var p = points.get(i);
             if (getY.applyAsDouble(start) > getY.applyAsDouble(p)) {
                 start = p;
+                start_idx = i;
             }
         }
 
         final var final_start = start;
-        ps.sort((a,b)->{
-            var comp_a = relativeAngle(final_start, a,getX,getY) - relativeAngle(final_start,b,getX,getY);
-            if(comp_a == 0){
-                var comp_d = distance(a,final_start, getX,getY) - distance(b,final_start, getX,getY);
-                return comp_d < 0 ? - 1 : comp_d > 0 ? 1 : 0;
+        swap(points, start_idx, points.size() - 1);
+        points.remove(points.size() - 1);
+        points.sort((a, b) -> {
+            var comp_a = ccw(final_start, a, b, getX, getY);
+            if (comp_a == 0) {
+                var comp_d = distance(a, final_start, getX, getY) - distance(b, final_start, getX, getY);
+                return comp_d < 0 ? -1 : comp_d > 0 ? 1 : 0;
             }
-            else return comp_a < 0 ? - 1 : comp_a > 0 ? 1 : 0;
+            else return -comp_a < 0 ? -1 : -comp_a > 0 ? 1 : 0;
         });
-        // ignore empty body
-        int s = ps.size() - 2;
-        while (s >= 0 & ccw(start, ps.get(s), ps.get(ps.size()-1), getX, getY) == 0) {
+        List<E> temp = new ArrayList<>(points.size() + 1);
+        temp.add(start);
+        temp.addAll(points);
+        points = temp;
+        int s = points.size() - 1;
+        while (s >= 0 && ccw(start, points.get(s), points.get(points.size() - 1), getX, getY) == 0) {
             s--;
         }
         s++;
 
-        for (int e = ps.size() - 1; e < s; e--, s++) {
-            swap(ps,s,e);
+        for (int e = points.size() - 1; s < e; e--, s++) {
+            swap(points, s, e);
         }
 
         Deque<E> points_stack = new ArrayDeque<>();
-        points_stack.addLast(ps.get(0));
-        points_stack.addLast(ps.get(1));
+
+        points_stack.addLast(points.get(0));
+        points_stack.addLast(points.get(1));
 
 
-        for (int i = 2; i < ps.size(); i++) {
-            var p = ps.get(i);
+        for (int i = 2; i < points.size(); i++) {
+            var p = points.get(i);
             var last = points_stack.removeLast();
-            var ccw = ccw(points_stack.getLast(), last, p, getX, getY);
-            if (ccw > 0) {
-                points_stack.addLast(last);
-                points_stack.addLast(p);
+            while (!points_stack.isEmpty() && ccw(points_stack.getLast(), last, p, getX, getY) < 0) {
+                last = points_stack.removeLast();
             }
-            else if (ccw == 0) {
-                points_stack.addLast(last);
-            }
-            else {
-                do {
-                    last = points_stack.removeLast();
-                }
-                while (ccw(points_stack.getLast(), last, p, getX, getY) < 0);
-                points_stack.addLast(last);
-                points_stack.addLast(p);
-            }
+            points_stack.addLast(last);
+            points_stack.addLast(p);
         }
 
         List<E> res = new ArrayList<>(points_stack.size());
@@ -72,19 +71,10 @@ public class ConvexHull {
         return res;
     }
 
-    private static <E> void swap(List<E> points, int i, int j){
-        var t=  points.get(i);
-        points.set(i,points.get(j));
-        points.set(j,t);
-    }
-
-    private static <E> double relativeAngle(E from, E to, ToDoubleFunction<E> getX, ToDoubleFunction<E> getY) {
-        var angle = Math.atan2(getY.applyAsDouble(to) - getY.applyAsDouble(from),
-                getX.applyAsDouble(to) - getX.applyAsDouble(from));
-        if (angle < 0) {
-            angle += Math.PI;
-        }
-        return angle;
+    private static <E> void swap(List<E> points, int i, int j) {
+        var t = points.get(i);
+        points.set(i, points.get(j));
+        points.set(j, t);
     }
 
     private static <E> double distance(E a, E b, ToDoubleFunction<E> getX, ToDoubleFunction<E> getY) {
@@ -93,10 +83,10 @@ public class ConvexHull {
 
         double bx = getX.applyAsDouble(b);
         double by = getY.applyAsDouble(b);
-        return Math.sqrt(Math.pow(ax - bx, 2) + Math.pow(ay - by, 2));
+        return Math.pow(ax - bx, 2) + Math.pow(ay - by, 2);
     }
 
-    private static <E> int ccw(E a, E b, E c, ToDoubleFunction<E> getX, ToDoubleFunction<E> getY) {
+    private static <E> double ccw(E a, E b, E c, ToDoubleFunction<E> getX, ToDoubleFunction<E> getY) {
         double ax = getX.applyAsDouble(a);
         double ay = getY.applyAsDouble(a);
 
@@ -105,16 +95,7 @@ public class ConvexHull {
 
         double cx = getX.applyAsDouble(c);
         double cy = getY.applyAsDouble(c);
-
-        double area = (bx - ax) * (cy - ay) - (by - ay) * (cx - ax);
-        if (area < 0) {
-            return -1;
-        }
-        else if (area > 0) {
-            return 1;
-        }
-        else {
-            return 0;
-        }
+        // (b[0] - a[0]) * (c[1] - b[1]) - (b[1] - a[1]) * (c[0] - b[0]);
+        return (bx - ax) * (cy - by) - (by - ay) * (cx - bx);
     }
 }
